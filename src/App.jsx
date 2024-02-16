4c1d421975d8b2e69edbe8fac617932bee64708c
Vikas Thakur <83107505+vikasthakur70183@users.noreply.github.com>
2024-02-16 17:29:13 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='modal-wrapper'></div>
+      <div className="main_div">
+        <h1>{count}</h1>
+        <div className='Div_btn' >
+          {count < 20 && <button className="btn" onClick={() => setCount((count) => count + 1)}>+</button>}
+          {count > 0 && <button className="btn" onClick={() => setCount((count) => count - 1)}>-</button>}
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
